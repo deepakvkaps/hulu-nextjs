f968aca317b4a3ebbddce9f295a23a1c348c3549
deepak <deepak.rajak@vkaps.com>
2021-08-31 00:45:13 +0530

    third commit

--- components/Results.js
+++ components/Results.js
@@ -3,8 +3,8 @@ import FlipMove from "react-flip-move"
 function Results({results}) {
     return (
         <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
-            {results.map(results => (
-                <Thumbnail key={results.id} results={results}/>
+            {results.map(result => (
+                <Thumbnail key={result.id} result={result}/>
             ))}
         </FlipMove>
     )
